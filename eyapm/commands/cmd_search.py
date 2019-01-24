8604a4926d944fb6007da20d97f625228cba8853
riag <riag@163.com>
2019-01-24 11:30:34 +0800

    search 命令加入支持查找文件名

--- eyapm/commands/cmd_search.py
+++ eyapm/commands/cmd_search.py
@@ -4,14 +4,8 @@ from pycman import config
 import eyapm
 
 
-@click.command('search')
-@click.pass_context
-@click.option(
-    '--config-file', 'config_file',
-    default=eyapm.default.config_file)
-@click.argument('pkgname', nargs=1, required=True)
-def cli(ctx, config_file, pkgname):
-    handle = config.init_with_config(config_file)
+def search_pkgname(handle, pkgname):
+
     local_db = handle.get_localdb()
     repos = handle.get_syncdbs()
     pkglist = []
@@ -28,3 +22,35 @@ def cli(ctx, config_file, pkgname):
             name, pkg.version,
             pkg.db.name, pkg.desc
         ))
+
+
+def search_filenames(handle, filenames):
+    localpkgs = handle.get_localdb().pkgcache
+
+    for name in filenames:
+        for pkg in localpkgs:
+            files = pkg.files
+            for fp in files:
+                if name in fp[0]:
+                    print("/%s is owned by %s" % (
+                        fp[0], pkg.name
+                    ))
+
+
+@click.command('search')
+@click.pass_context
+@click.option(
+    '--config-file', 'config_file',
+    default=eyapm.default.config_file)
+@click.option(
+    '--files', 'search_files', is_flag=True, default=False
+)
+@click.argument('pkgname_or_filename', nargs=-1, required=True)
+def cli(ctx, config_file, search_files, pkgname_or_filename):
+    handle = config.init_with_config(config_file)
+
+    if not search_files:
+        search_pkgname(handle, pkgname_or_filename[0])
+        return
+
+    search_filenames(handle, pkgname_or_filename)
